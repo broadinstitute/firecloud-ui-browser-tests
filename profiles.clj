9c0302bc174dc7ed13b8481410e47164ebbc18ab
Greg Rushton <grushton@broadinstitute.org>
2015-07-16 15:32:41 -0400

    Move the linux test to a docker image

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,5 @@
 {
- :linux {:jvm-opts ["-Dwebdriver.chrome.driver=./target/chromedriver"]}
+ :docker {:jvm-opts ["-Dwebdriver.chrome.driver=/opt/selenium/chromedriver-2.16"]}
  :mac {:jvm-opts ["-Dwebdriver.chrome.driver=./target/chromedriver"]}
  :win {:jvm-opts ["-Dwebdriver.chrome.driver=./target/chromedriver.exe"]}
  }
